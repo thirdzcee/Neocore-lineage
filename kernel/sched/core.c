2123d6d6fc964d508134a2880a0ce1d037d325f2
ironbuang <thirdzcee@github.com>
2018-07-24 07:02:44 +0800

    fix redefinition

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -95,26 +95,6 @@
 #include <trace/events/sched.h>
 #include "walt.h"
 
-void start_bandwidth_timer(struct hrtimer *period_timer, ktime_t period)
-{
-	unsigned long delta;
-	ktime_t soft, hard, now;
-
-	for (;;) {
-		if (hrtimer_active(period_timer))
-			break;
-
-		now = hrtimer_cb_get_time(period_timer);
-		hrtimer_forward(period_timer, now, period);
-
-		soft = hrtimer_get_softexpires(period_timer);
-		hard = hrtimer_get_expires(period_timer);
-		delta = ktime_to_ns(ktime_sub(hard, soft));
-		__hrtimer_start_range_ns(period_timer, soft, delta,
-					 HRTIMER_MODE_ABS_PINNED, 0);
-	}
-}
-
 static atomic_t __su_instances;
 
 int su_instances(void)
