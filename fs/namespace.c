d5a2e93d8a95693fb54a8b6e85accbf798e14847
ironbuang <thirdzcee@github.com>
2018-08-12 23:31:37 +0800

    Revert "fs: set noatime flag as default"
    This reverts commit c5c1dad1f5cfefe16ab5d359c85fcc49fc879cf4.

--- fs/namespace.c
+++ fs/namespace.c
@@ -2641,8 +2641,8 @@ long do_mount(const char *dev_name, const char __user *dir_name,
 		goto dput_out;
 
 	/* Default to relatime unless overriden */
-	if (!(flags & MNT_RELATIME))
-		mnt_flags |= MS_NOATIME;
+	if (!(flags & MS_NOATIME))
+		mnt_flags |= MNT_RELATIME;
 
 	/* Separate the per-mountpoint flags */
 	if (flags & MS_NOSUID)
